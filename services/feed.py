45069b93f965702491b0dee027de6b1bb3646733
domenico.devito <domenico.devito@planetwatch.io>
2024-04-23 16:01:41 +0200

    fix: Filter podcasts by a set limit

--- services/feed.py
+++ services/feed.py
@@ -6,6 +6,7 @@ from django.utils.feedgenerator import Rss201rev2Feed
 
 from podcast.models import Podcast
 
+PODCAST_LIMIT: int = int(os.getenv("PODCAST_LIMIT", 150))
 AWS_ACCESS_KEY = os.getenv("AWS_ACCESS_KEY")
 AWS_SECRET_KEY = os.getenv("AWS_SECRET_KEY")
 BUCKET_NAME = os.getenv("BUCKET_NAME")
@@ -77,7 +78,7 @@ class PodcastFeed(Feed):
     language = "it"
 
     def items(self):
-        return Podcast.objects.all().order_by("-insert_time")
+        return Podcast.objects.all().order_by("-insert_time")[:PODCAST_LIMIT]
 
     def item_title(self, item):
         return item.title
