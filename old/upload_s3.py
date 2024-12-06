5755cef80ad2da9771a351afccec4ebdf0a24b88
keewee <kinycx@gmail.com>
2024-12-06 10:34:27 +0100

    fix: Update S3 upload script to use correct environment variable names and improve file path handling

--- old/upload_s3.py
+++ old/upload_s3.py
@@ -8,9 +8,9 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # Access environment variables
-aws_access_key_id = os.getenv("AWS_ACCESS_KEY_ID")
-aws_secret_access_key = os.getenv("AWS_SECRET_ACCESS_KEY")
-aws_bucket_name = os.getenv("AWS_BUCKET_NAME")
+aws_access_key_id = os.getenv("AWS_ACCESS_KEY")
+aws_secret_access_key = os.getenv("AWS_SECRET_KEY")
+aws_bucket_name = os.getenv("BUCKET_NAME")
 
 # Initialize S3 client
 s3_client = boto3.client(
@@ -33,7 +33,6 @@ with open("feed.json", "r", encoding="utf-8") as json_file:
 total_items = len(data["items"])
 failed_uploads = []
 
-
 # Function to check if a file exists in the S3 bucket
 def file_exists_in_s3(bucket_name, s3_key):
     try:
@@ -45,29 +44,26 @@ def file_exists_in_s3(bucket_name, s3_key):
         else:
             raise
 
-
 # Upload files to S3 and log progress
 for index, item in enumerate(data["items"]):
     try:
         # Upload audio file if guid exists
         if "guid" in item and item["guid"]:
-            audio_filename = os.path.join(
-                local_audio_path, os.path.basename(item["guid"])
-            )
-            s3_key = f"{audio_upload_folder}{os.path.basename(audio_filename)}"
+            audio_filename = os.path.basename(item["guid"])
+            local_audio_filepath = os.path.join(local_audio_path, audio_filename)
+            s3_key = f"{audio_upload_folder}{audio_filename}"
             if file_exists_in_s3(aws_bucket_name, s3_key):
                 print(f"File {s3_key} already exists in S3, overwriting...")
-            s3_client.upload_file(audio_filename, aws_bucket_name, s3_key)
+            s3_client.upload_file(local_audio_filepath, aws_bucket_name, s3_key)
 
         # Upload cover image if image exists
         if "image" in item and item["image"]:
-            image_filename = os.path.join(
-                local_cover_path, os.path.basename(item["image"])
-            )
-            s3_key = f"{cover_upload_folder}{os.path.basename(image_filename)}"
+            image_filename = os.path.basename(item["image"])
+            local_image_filepath = os.path.join(local_cover_path, image_filename)
+            s3_key = f"{cover_upload_folder}{image_filename}"
             if file_exists_in_s3(aws_bucket_name, s3_key):
                 print(f"File {s3_key} already exists in S3, overwriting...")
-            s3_client.upload_file(image_filename, aws_bucket_name, s3_key)
+            s3_client.upload_file(local_image_filepath, aws_bucket_name, s3_key)
 
         # Log progress
         progress = (index + 1) / total_items * 100
@@ -76,7 +72,7 @@ for index, item in enumerate(data["items"]):
     except Exception as e:
         failed_uploads.append({"item": item, "error": str(e)})
         print(
-            f"Failed to upload {item.get('guid', 'N/A')} or {item.get('image', 'N/A')}: {e}"
+            f"\033[91mFailed to upload {item.get('guid', 'N/A')} or {item.get('image', 'N/A')}: {e}\033[0m"
         )
 
 # Save failed uploads to a file
