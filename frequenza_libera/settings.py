004762ed4169a79cd53e0c3487941cf2ca90e7d9
keewee <kinycx@gmail.com>
2024-12-08 23:18:48 +0100

    feat: Refactor allowed hosts and CSRF trusted origins settings in settings.py

--- frequenza_libera/settings.py
+++ frequenza_libera/settings.py
@@ -37,6 +37,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # CORS settings
 CORS_ALLOW_ALL_ORIGINS = env.bool("CORS_ALLOW_ALL_ORIGINS", default=False)
 CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
+# Set allowed hosts for production
+# ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["yourdomain.com"])
+ALLOWED_HOSTS = ["testserver", env("PROD_HOST"), "localhost", "127.0.0.1"]
+CSRF_TRUSTED_ORIGINS = [f"https://{env('PROD_HOST')}"]
+
+# Or to allow specific origins:
+# CORS_ALLOWED_ORIGINS = [
+#     "https://example.com",
+#     "https://sub.example.com",
+# ]
 
 # Application definition
 INSTALLED_APPS = [
@@ -90,17 +100,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "frequenza_libera.wsgi.application"
-
-# Set allowed hosts for production
-# ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["yourdomain.com"])
-ALLOWED_HOSTS = ["testserver", env("PROD_HOST"), "localhost", "127.0.0.1"]
-CSRF_TRUSTED_ORIGINS = [f"https://{env('PROD_HOST')}"]
-
-# Or to allow specific origins:
-# CORS_ALLOWED_ORIGINS = [
-#     "https://example.com",
-#     "https://sub.example.com",
-# ]
 
 # Database
 DATABASES = {
