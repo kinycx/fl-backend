7f8593f1a6c04f9e74c379042d307b9e56d234b9
keewee <kinycx@gmail.com>
2025-02-17 13:00:58 +0100

    chore: Add additional dependencies to replit.nix for enhanced functionality

--- replit.nix
+++ replit.nix
@@ -1,5 +1,15 @@
 {pkgs}: {
   deps = [
+    pkgs.tk
+    pkgs.tcl
+    pkgs.openjpeg
+    pkgs.libxcrypt
+    pkgs.libwebp
+    pkgs.libtiff
+    pkgs.libjpeg
+    pkgs.libimagequant
+    pkgs.lcms2
+    pkgs.freetype
     pkgs.cacert
     pkgs.pgadmin4
     pkgs.zlib
