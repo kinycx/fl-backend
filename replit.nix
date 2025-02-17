b12617949a6ddfd7b01be1e1dad643e09c7d819e
keewee <kinycx@gmail.com>
2025-02-17 16:40:24 +0100

    chore: Update replit.nix to remove unnecessary dependencies and add PostgreSQL and Python packages

--- replit.nix
+++ replit.nix
@@ -1,19 +1,9 @@
 {pkgs}: {
   deps = [
-    pkgs.tk
-    pkgs.tcl
-    pkgs.openjpeg
-    pkgs.libxcrypt
-    pkgs.libwebp
-    pkgs.libtiff
-    pkgs.libjpeg
-    pkgs.libimagequant
-    pkgs.lcms2
-    pkgs.freetype
-    pkgs.cacert
-    pkgs.pgadmin4
-    pkgs.zlib
-    pkgs.xcodebuild
     pkgs.postgresql
+    pkgs.python3
+    pkgs.python3Packages.psycopg
+    pkgs.gcc
+    pkgs.pkg-config
   ];
 }
