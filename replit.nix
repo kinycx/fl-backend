484d73034be050e7f09f05cc4cff86fae1a093b9
keewee <kinycx@gmail.com>
2025-02-18 14:42:40 +0100

    chore: Update .replit and replit.nix for improved deployment configuration and module management

--- replit.nix
+++ replit.nix
@@ -1,26 +1,5 @@
 {pkgs}: {
   deps = [
-    pkgs.cacert
-    pkgs.zlib
-    pkgs.tk
-    pkgs.tcl
-    pkgs.openjpeg
-    pkgs.libxcrypt
-    pkgs.libwebp
-    pkgs.libtiff
-    pkgs.libjpeg
-    pkgs.libimagequant
-    pkgs.lcms2
-    pkgs.freetype
-    pkgs.pgadmin4
-    pkgs.libpg_query
-    pkgs.libpqxx
-    pkgs.libpqxx_6
     pkgs.postgresql
-    pkgs.python3
-    pkgs.python3Packages.psycopg
-    pkgs.gcc
-    pkgs.pkg-config
-    pkgs.xcodebuild
   ];
 }
