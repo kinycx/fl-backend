ba0d7decff8f13cbbdd41bd13d4c8cf385be31de
keewee <kinycx@gmail.com>
2025-04-23 16:23:12 +0200

    fix: Order podcast collection by update time

--- podcast_collection/views.py
+++ podcast_collection/views.py
@@ -11,5 +11,5 @@ from .models import PodcastCollection, PodcastCollectionSerializer
 class PodcastCollectionListCreateView(ListCreateAPIView):
     """View for listing and creating Podcasts (GET, POST)"""
 
-    queryset = PodcastCollection.objects.all()
+    queryset = PodcastCollection.objects.all().order_by("-update_time")
     serializer_class = PodcastCollectionSerializer
